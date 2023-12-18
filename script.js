e8c5c2e177af42da0b7cffebedb533a3cb9c94c4
uttamkn <uttamkn15@gmail.com>
2023-12-18 22:32:52 +0530

    Add a user prompt to ask the number of questions to be generated

--- script.js
+++ script.js
@@ -2,82 +2,111 @@ var res=0;
 var myArr;
 var y;
 var qno=0;
+var userInput=0;
 
 
-function lock(x) {
-    switch(x) {
-        case 1: {y=x;if(myArr[x]==myArr[5].substring(8)){document.getElementById("op2").style.backgroundColor="#eee";document.getElementById("op3").style.backgroundColor="#eee";document.getElementById("op4").style.backgroundColor="#eee";document.getElementById("op1").style.backgroundColor="#097969"; } else {document.getElementById("op2").style.backgroundColor="#eee";document.getElementById("op3").style.backgroundColor="#eee";document.getElementById("op4").style.backgroundColor="#eee";document.getElementById("op1").style.backgroundColor="red";}
-        check();
-        next();}
-        break;
-        case 2: {y=x;if(myArr[x]==myArr[5].substring(8)){document.getElementById("op1").style.backgroundColor="#eee";document.getElementById("op3").style.backgroundColor="#eee";document.getElementById("op4").style.backgroundColor="#eee";document.getElementById("op2").style.backgroundColor="#097969"; } else {document.getElementById("op1").style.backgroundColor="#eee";document.getElementById("op3").style.backgroundColor="#eee";document.getElementById("op4").style.backgroundColor="#eee";document.getElementById("op2").style.backgroundColor="red";}
-        check();
-        next();}
-        break;
-        case 3: {y=x;if(myArr[x]==myArr[5].substring(8)){document.getElementById("op2").style.backgroundColor="#eee";document.getElementById("op1").style.backgroundColor="#eee";document.getElementById("op4").style.backgroundColor="#eee";document.getElementById("op3").style.backgroundColor="#097969"; } else {document.getElementById("op2").style.backgroundColor="#eee";document.getElementById("op1").style.backgroundColor="#eee";document.getElementById("op4").style.backgroundColor="#eee";document.getElementById("op3").style.backgroundColor="red";}
-        check();
-        next();}
-        break;
-        case 4: {y=x;if(myArr[x]==myArr[5].substring(8)){document.getElementById("op2").style.backgroundColor="#eee";document.getElementById("op3").style.backgroundColor="#eee";document.getElementById("op1").style.backgroundColor="#eee";document.getElementById("op4").style.backgroundColor="#097969"; } else {document.getElementById("op2").style.backgroundColor="#eee";document.getElementById("op3").style.backgroundColor="#eee";document.getElementById("op1").style.backgroundColor="#eee";document.getElementById("op4").style.backgroundColor="red";}
-        check();
-        next();}
-        break;
-    }
+// to display questions (starts the quiz)
+if(qno==0){
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const toggleButton = document.getElementById("start-button");
+    const elements = document.querySelectorAll(".hide");
+
+    toggleButton.addEventListener("click", function () {
+        document.getElementById("menu").style.display = "none";
+        elements.forEach(function (element) {
+            element.style.display = "flex";
+        });
+    });
+});
 }
+
+
+function takeInput() {
+  userInput = document.getElementById("maxq").value
+  setTimeout (next(), 200);
+}
+
+
+
+function lock(selectedOption) {
+  y = selectedOption;
+
+  // Reset background colors for all options
+  for (let i = 1; i <= 4; i++) {
+      document.getElementById(`op${i}`).style.backgroundColor = "#eee";
+  }
+
+  // green if correct and red if wrong
+  if (myArr[selectedOption]==myArr[5].substring(8)){
+      document.getElementById(`op${selectedOption}`).style.backgroundColor = "#097969";
+  }
+  else {
+      document.getElementById(`op${selectedOption}`).style.backgroundColor = "#ff7f7f";
+  }
+  // Check the answer and proceed to the next question
+  setTimeout(check, 200);
+  setTimeout(next2, 900);
+}
+
+
+
 
 // const fetch = require('node-fetch');
 
 const apiBase = "https://api.endpoints.anyscale.com/v1";
-const token = "token";
+const token = "esecret_nu9jkw8amvtfrshqjnqn38ahgz";
 const url = `${apiBase}/chat/completions`;
 var apiResponse;
 
 const body = {
   model: "mistralai/Mistral-7B-Instruct-v0.1",
   messages: [
-    { role: "system", content: "You are quiz questions generating bot which generates simple and understandable quiz questions." },
-    { role: "user", content: "Generate a quiz question related to any field like science, technology, history, etc. Follow the template: 'Question: <question goes here>?\nA) <Option 1>\nB) <Option 2>\nC) <Option 3>\nD) <Option 4>\nAnswer: <option letter>) <correct option>'" }
+    { role: "system", content: "You are question generating bot which generates understandable quiz questions according to the given template." },
+    { role: "user", content: "Generate one unique question related to maths, technology or science. Follow the template: 'Question: <question goes here>?\nA) <Option 1>\nB) <Option 2>\nC) <Option 3>\nD) <Option 4>\nAnswer: <option letter>) <correct option>' ensure you give the correct option letter with the corresponding option" }
   ],
   temperature: 0.7
 };
 
 function next() {
+  
+  
    document.getElementById("op1").style.backgroundColor="#eee";
    document.getElementById("op2").style.backgroundColor="#eee";
    document.getElementById("op3").style.backgroundColor="#eee";
    document.getElementById("op4").style.backgroundColor="#eee";
 
-qno++;
-document.getElementById("qno").innerHTML="Question "+qno+":";
+   qno++;
+   document.getElementById("qno").innerHTML="Question "+qno+":";
 
-fetch(url, {
-  method: 'POST',
-  headers: {
-    'Authorization': `Bearer ${token}`,
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify(body),
-})
-  .then(response => response.json())
-  .then(data => {apiResponse = data.choices[0].message.content; console.log(apiResponse); myArr=apiResponse.split('\n'); document.getElementById("ques").innerHTML= myArr[0].substring(10); document.getElementById("op1").innerHTML= myArr[1]; document.getElementById("op2").innerHTML= myArr[2]; document.getElementById("op3").innerHTML= myArr[3]; document.getElementById("op4").innerHTML= myArr[4];})
-  .catch(error => console.error('Error:', error));
 
-}
+   fetch(url, {
+     method: 'POST',
+     headers: {
+        'Authorization': `Bearer ${token}`,
+       'Content-Type': 'application/json',
+     },
+      body: JSON.stringify(body),
+    })
+      .then(response => response.json())
+      .then(data => {apiResponse = data.choices[0].message.content; console.log(apiResponse); myArr=apiResponse.split('\n'); document.getElementById("ques").innerHTML= myArr[0].substring(10); document.getElementById("op1").innerHTML= myArr[1]; document.getElementById("op2").innerHTML= myArr[2]; document.getElementById("op3").innerHTML= myArr[3]; document.getElementById("op4").innerHTML= myArr[4];})
+      .catch(error => console.error('Error:', error));
+    }
+
 
 function check() {
+  if (myArr && myArr.length > 5) {  // Check if myArr is defined and has enough elements
 
-  if(qno<5){
-  document.getElementById("next").innerHTML="next";
+      if (myArr[y] == myArr[5].substring(8)) {
+          res += 10;
+      }
+      document.getElementById("res").innerHTML = "Score: " + res;
+  } else {
+      console.error("WARNING: myArr is undefined or has insufficient elements.");
   }
-
-  if(myArr[y]==myArr[5].substring(8)){
-    res+=10;
-  };
-  document.getElementById("res").innerHTML="Score: "+res;
 }
 
 function submit() {
-  // Assuming res is a global variable
 
   // Set the "res" value in the "thanks.html" file
   localStorage.setItem('finalScore', res);
@@ -97,6 +126,12 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 });
 
+function next2() {
 
-
-
+  if(qno < Number(userInput)) {
+    next();
+  }
+  else {
+    submit();
+  }
+}
