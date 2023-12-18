004e0a3079d1e74e75b4c5fd512ec945a90dc574
uttamkn <uttamkn15@gmail.com>
2023-12-18 22:35:51 +0530

    add user prompt

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ function lock(selectedOption) {
 // const fetch = require('node-fetch');
 
 const apiBase = "https://api.endpoints.anyscale.com/v1";
-const token = "esecret_nu9jkw8amvtfrshqjnqn38ahgz";
+const token = "your-token";
 const url = `${apiBase}/chat/completions`;
 var apiResponse;
 
