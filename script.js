697ee3e407c199a70919689fc5def9d6fca09db6
uttamkn <uttamkn15@gmail.com>
2024-01-07 20:10:32 +0530

    dump

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ function lock(selectedOption) {
 // const fetch = require('node-fetch');
 
 const apiBase = "https://api.endpoints.anyscale.com/v1";
-const token = "YSour-token";
+const token = "your-token";
 const url = `${apiBase}/chat/completions`;
 var apiResponse;
 
